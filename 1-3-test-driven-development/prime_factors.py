5442426515efe787bc0c1954b13732027a0ac3be
Oskar Korczak <oskarissimus@gmail.com>
2021-02-08 23:55:55 +0100

    first iteration!

--- 1-3-test-driven-development/prime_factors.py
+++ 1-3-test-driven-development/prime_factors.py
@@ -1,8 +1,10 @@
 import pytest
-
+ 
 def prime_factors(number):
+    if type(number) != int:
+        raise TypeError()   
     return
 
 def test_type():
     with pytest.raises(TypeError):
-        prime_factors(None) 
+        prime_factors(None)
