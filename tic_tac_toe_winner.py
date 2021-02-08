7e8c4e8b1b2c9d8026e8ec4348f5897c74004009
Oskar Korczak <oskarissimus@gmail.com>
2021-02-08 20:06:53 +0100

    example from site

--- tic_tac_toe_winner.py
+++ tic_tac_toe_winner.py
@@ -1,2 +1,14 @@
-def tic_tac_toe_winner(board_view):
-    return
+
+
+def tic_tac_toe_winner(board):
+    return True
+
+test_cases = {
+    'XO  X O X': 'X',
+    'OX  O X O': 'O',
+    'XXOOXXXOO': None
+}
+
+for board, expectation in test_cases.items():
+    assert tic_tac_toe_winner(board) == expectation, \
+        f'Expected {expectation!r} for {board!r} got {response!r}'
