8cd0b618b260e9c34af85634f15d7bc09bdf5ad2
Oskar Korczak <oskarissimus@gmail.com>
2021-02-08 23:01:07 +0100

    function and some tests working

--- 1-1-po-co-testowac/is_palindrome.py
+++ 1-1-po-co-testowac/is_palindrome.py
@@ -1,2 +1,38 @@
 def is_palindrome(data):
-    return
+    if type(data) != str:
+        raise TypeError()
+
+    if data == '':
+        raise ValueError()
+
+    left = 0
+    right = len(data)-1
+    while left < right:
+        if data[left] != data[right]:
+            return False
+        left  += 1
+        right -= 1
+    return True
+
+test_cases = {
+    ''   : ValueError,
+    10   : TypeError,
+    'aba': True,
+    'abba': True,
+    'abcba': True,
+    'abca': False,
+}
+
+for data, expectation in test_cases.items():
+    print ('----------')
+    print (type(expectation))
+    if expectation in (ValueError,TypeError):
+        try:
+            response = is_palindrome(data)
+            print(f'Expected {expectation!r} for {data!r} got {response!r}')
+        except expectation:
+            pass
+    else:
+        response = is_palindrome(data)
+        assert response == expectation, \
+            f'Expected {expectation!r} for {data!r} got {response!r}'
