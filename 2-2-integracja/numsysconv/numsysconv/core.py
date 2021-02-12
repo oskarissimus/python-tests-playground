48a23e031ee0d0796be5c5039073044f50ff0a07
Oskar Korczak <oskarissimus@gmail.com>
2021-02-13 00:35:04 +0100

    numeral system naming validation implemented

--- 2-2-integracja/numsysconv/numsysconv/core.py
+++ 2-2-integracja/numsysconv/numsysconv/core.py
@@ -1,6 +1,15 @@
 def convert_numeral_system(source_numeral_system,
                            target_numeral_system,
                            number):
+    if source_numeral_system not in ['decimal','roman']:
+        raise ValueError('source_numeral_system has to be "decimal" or "roman"')
+
+    if target_numeral_system not in ['decimal','roman']:
+        raise ValueError('target_numeral_system has to be "decimal" or "roman"')
+
     if number == 1:
         return 'I'
+    
+    if number == 'I':
+        return 1
     
--- 2-2-integracja/numsysconv/tests/test_core.py
+++ 2-2-integracja/numsysconv/tests/test_core.py
@@ -6,36 +6,41 @@ def test_convertion_of_one_from_decimal_to_roman():
                                   target_numeral_system='roman',
                                   number=1) == 'I'
 
+def test_convertion_of_one_from_roman_to_decimal():
+    assert convert_numeral_system(source_numeral_system='roman',
+                                  target_numeral_system='decimal',
+                                  number='I') == 1
+
 class TestNumeralSystemNamingValidation:
 
-    def test_source_numeral_system_naming_validation_accept_decimal():
+    def test_source_numeral_system_naming_validation_accept_decimal(self):
         assert convert_numeral_system(source_numeral_system='decimal',
                                       target_numeral_system='roman',
                                       number=1) == 'I'
 
-    def test_source_numeral_system_naming_validation_accept_roman():
+    def test_source_numeral_system_naming_validation_accept_roman(self):
         assert convert_numeral_system(source_numeral_system='roman',
                                       target_numeral_system='decimal',
                                       number='I') == 1
 
-    def test_source_numeral_system_naming_validation_reject_bad_name():
+    def test_source_numeral_system_naming_validation_reject_bad_name(self):
         with pytest.raises(ValueError):
             convert_numeral_system(source_numeral_system='deci=====mal',
                                    target_numeral_system='roman',
                                    number=1)
 
 
-    def test_target_numeral_system_naming_validation_accept_decimal():
+    def test_target_numeral_system_naming_validation_accept_decimal(self):
         assert convert_numeral_system(source_numeral_system='decimal',
                                       target_numeral_system='roman',
                                       number=1) == 'I'
 
-    def test_target_numeral_system_naming_validation_accept_roman():
+    def test_target_numeral_system_naming_validation_accept_roman(self):
         assert convert_numeral_system(source_numeral_system='roman',
                                       target_numeral_system='decimal',
                                       number='I') == 1
 
-    def test_target_numeral_system_naming_validation_reject_bad_name():
+    def test_target_numeral_system_naming_validation_reject_bad_name(self):
         with pytest.raises(ValueError):
             convert_numeral_system(source_numeral_system='roman',
                                    target_numeral_system='deci=====mal',
