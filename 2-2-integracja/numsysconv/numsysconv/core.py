8e479f4fe031582f4b17e180687a670b813a4fec
Oskar Korczak <oskarissimus@gmail.com>
2021-02-13 00:56:27 +0100

    checking compatibility of number type with numeral system implemented

--- 2-2-integracja/numsysconv/numsysconv/core.py
+++ 2-2-integracja/numsysconv/numsysconv/core.py
@@ -1,7 +1,7 @@
 def convert_numeral_system(source_numeral_system,
                            target_numeral_system,
                            number):
-    #validating imput
+    #validating input
     if source_numeral_system not in ['decimal','roman']:
         raise ValueError('source_numeral_system has to be "decimal" or "roman"')
 
@@ -10,6 +10,13 @@ def convert_numeral_system(source_numeral_system,
 
     if source_numeral_system == target_numeral_system:
         raise ValueError('target_numeral_system and source_numeral_system must differ')
+
+    if source_numeral_system == 'decimal' and not isinstance(number,int):
+        raise TypeError('converting from decimal - number must be int')
+
+    if source_numeral_system == 'roman' and not isinstance(number,str):
+        raise TypeError('converting from roman numeral system - number must be string')
+
 
     #some placeholders
     if number == 1:
