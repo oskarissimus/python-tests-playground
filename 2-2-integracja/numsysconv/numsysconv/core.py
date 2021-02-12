7ca0ace56b1304a7a9f0101ff6f4f20edec20e18
Oskar Korczak <oskarissimus@gmail.com>
2021-02-13 00:12:57 +0100

    jaki test taka implementacja

--- 2-2-integracja/numsysconv/numsysconv/core.py
+++ 2-2-integracja/numsysconv/numsysconv/core.py
@@ -1,2 +1,6 @@
-def convert_numeral_system():
-    return
+def convert_numeral_system(source_numeral_system,
+                           destination_numeral_system,
+                           number):
+    if number == 1:
+        return 'I'
+    
