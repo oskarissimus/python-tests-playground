43ecc2cc5ad33b48f596d967e080d3184cc152d4
Oskar Korczak <oskarissimus@gmail.com>
2021-02-13 00:44:56 +0100

    rejection of pointless convertions implemented

--- 2-2-integracja/numsysconv/numsysconv/core.py
+++ 2-2-integracja/numsysconv/numsysconv/core.py
@@ -1,12 +1,17 @@
 def convert_numeral_system(source_numeral_system,
                            target_numeral_system,
                            number):
+    #validating imput
     if source_numeral_system not in ['decimal','roman']:
         raise ValueError('source_numeral_system has to be "decimal" or "roman"')
 
     if target_numeral_system not in ['decimal','roman']:
         raise ValueError('target_numeral_system has to be "decimal" or "roman"')
 
+    if source_numeral_system == target_numeral_system:
+        raise ValueError('target_numeral_system and source_numeral_system must differ')
+
+    #some placeholders
     if number == 1:
         return 'I'
     
